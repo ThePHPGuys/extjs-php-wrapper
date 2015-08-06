97cc26b466b525e534b94daf719ceae5645457e1
misterx <vadimx@gmail.com>
2015-08-06 13:16:18 +0300

    Added Text.editable

--- src/Form/Field/Text.php
+++ src/Form/Field/Text.php
@@ -11,6 +11,11 @@ namespace Ext\Form\Field;
 
 class Text extends Base
 {
+	/**
+	 * Specify false to validate that the value's length must be > 0
+	 * @param $allowBlank
+	 * @return $this
+	 */
 	public function setAllowBlank($allowBlank){
 		return $this->setProperty('allowBlank',$allowBlank);
 	}
@@ -19,6 +24,11 @@ class Text extends Base
 		return $this->getProperty('allowBlank');
 	}
 
+	/**
+	 * true if this field should automatically grow and shrink to its content
+	 * @param $grow
+	 * @return $this
+	 */
 	public function setGrow($grow){
 		return $this->setProperty('grow', $grow);
 	}
@@ -27,5 +37,18 @@ class Text extends Base
 		return $this->getProperty('grow');
 	}
 
+	/**
+	 * false to prevent the user from typing text directly into the field
+	 * @param $editable
+	 * @return $this
+	 */
+	public function setEditable($editable){
+		return $this->setProperty('editable',$editable);
+	}
+	
+	public function getEditable(){
+		return $this->getProperty('editable');
+	}
+	
 
 }
