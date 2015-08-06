6677ed3740d774e69abf7c0f05d925d9685e8e43
misterx <vadimx@gmail.com>
2015-08-06 09:21:37 +0300

    Added table properties

--- src/Panel/Table.php
+++ src/Panel/Table.php
@@ -6,4 +6,32 @@ use Ext\Util\StoreHolder;
 class Table extends Panel
 {
 	use StoreHolder;
+
+	public function setEnableColumnHide($enableColumnHide){
+		return $this->setProperty('enableColumnHide',$enableColumnHide);
+	}
+
+	public function getEnableColumnHide(){
+		return $this->getProperty('enableColumnHide');
+	}
+
+	public function setEnableColumnMove($enableColumnMove){
+		return $this->setProperty('enableColumnMove',$enableColumnMove);
+	}
+
+	public function getEnableColumnMove(){
+		return $this->getProperty('enableColumnMove');
+	}
+
+	public function setEnableColumnResize($enableColumnResize){
+		return $this->setProperty('enableColumnResize',$enableColumnResize);
+	}
+
+	public function getEnableColumnResize(){
+		return $this->getProperty('enableColumnResize');
+	}
+
+
+
+
 }
